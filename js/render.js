56378cf5eeba938de531fa6233193ee0f05bd1ab
Untitled-developerYT <102920258+Untitled-developerYT@users.noreply.github.com>
2025-01-23 23:15:12 +0000

    Update render.js

--- js/render.js
+++ js/render.js
@@ -15,7 +15,7 @@
   function display(xhr) {
     var parsed = reader.parse(xhr.responseText);
     var content = writer.renderBlock(parsed);
-    var finished = content.replace(/---.share: "True".---/gs,'');
+    var finished = content.replace(/---\sshare: "True"\s---/g,'');
     document.getElementsByTagName('body')[0].innerHTML = finished;
     
     /* try to extract h1 title and use as title for page
