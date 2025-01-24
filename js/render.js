23774a97e9d4b52930e25d0ef9493752e96282cf
Untitled-developerYT <102920258+Untitled-developerYT@users.noreply.github.com>
2025-01-24 23:28:47 +0000

    Update render.js

--- js/render.js
+++ js/render.js
@@ -18,7 +18,7 @@
     var parsed = reader.parse(clean);
     var content = writer.renderBlock(parsed);
     
-    document.getElementsByTagName('body')[0].innerHTML = content;
+    document.getElementsByTagId('content')[0].innerHTML = content;
     
     /* try to extract h1 title and use as title for page
        if no h1, use name of file 
